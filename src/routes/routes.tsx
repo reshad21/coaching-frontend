fa56f6b6f52efa6aaf798661cca22b7cba041768
reshad21 <miakala7@gmail.com>
2025-04-15 23:43:28 +0600

    pull from asif vai

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,24 +3,21 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import { navMain } from "@/components/Routs/Routs";
 import Test from "@/page/Test/Test";
-import PrivateRout from "@/PrivateRout/PrivateRout";
 import { LogIn } from "lucide-react";
 import { createBrowserRouter } from "react-router-dom";
-
 
 const componentMapping: Record<string, React.ElementType> = {
   // student routes
   "/test": Test,
-  
 };
 
 const routs = createBrowserRouter([
   {
     path: "/",
     element: (
-      <PrivateRout>
-        <MainLayout />
-      </PrivateRout>
+      // <PrivateRout>
+      <MainLayout />
+      // </PrivateRout>
     ),
     children: navMain.flatMap((section) =>
       section.items?.map((item: any) => {
